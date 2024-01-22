a9e665fc6c7dee5fe8ef448992c9c16d6e6688a4
Lewechi Godsfavour <lewechigodsfavour@gmail.com>
2024-01-22 04:07:18 +0100

    product details update

---
+++ lib/features/presentation/pages/product_details__screen/product_details_widget/product_details_appbar.dart
@@ -0,0 +1,31 @@
+import 'package:beauty_store/core/constant/color.dart';
+import 'package:beauty_store/core/constant/dimensions.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailsAppBar extends StatelessWidget {
+  const ProductDetailsAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: getProportionateScreenHeight(300),
+      width: SizeConfig.screenWidth,
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30)),
+          color: AppColors.blueshade200),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: getProportionateScreenWidth(50)),
+          Padding(
+            padding: EdgeInsets.only(left: getProportionateScreenWidth(25)),
+            child: const Icon(Icons.arrow_back_ios,
+                color: AppColors.blueshade400, size: 20),
+          ),
+        ],
+      ),
+    );
+  }
+}
